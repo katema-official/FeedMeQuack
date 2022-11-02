6ff045d19addc8cd513e0c20941a3be8deaeb1dc
Penzo00 <101516862+Penzo00@users.noreply.github.com>
2022-11-02 16:54:51 +0100

    First commit

---
+++ Assets/Music/L.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("From Music folder");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
