c224f5ee67266664184a1100cb9884cf34fb79ec
katema-official <alessio.lagreca13@gmail.com>
2022-11-04 10:26:53 +0100

    Leart something about scenes
    Basically: (1) if you want to load a scene, the first thing you have to do is go to File > Build Settings and drag and drop your scen in the window. In this way, you can call from code 'SceneManager.LoadScene(sceneName)' to go to that scene. (2) if you want a persistent object throughout scenes, you can create a MonoBehaviour (let's call it 'DontDestroy') that calls in the awake function 'DontDestroyOnLoad(gameObject);', so that the object to which this is attached is not destroyed when a new scene is loaded

---
+++ Assets/Dungeon/Scripts/Lakes/LakeDescriptionComponent.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LakeDescriptionComponent : MonoBehaviour
+{
+    //component that specifies the nature of a lake
+    [SerializeField] public int NumberOfMallard;
+    [SerializeField] public int NumberOfCoot;
+    [SerializeField] public int NumberOfGoose;
+    [SerializeField] public int NumberOfFish;       //we'll think about them in the future
+    [SerializeField] public int NumberOfSeagull;    //we'll think about them in the future
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
