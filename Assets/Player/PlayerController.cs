ca05033d70672cdf2eec58cc9f2df815ea47aae8
ivs-spec <ivs-spec@github.com>
2022-11-05 14:27:42 +0100

    Added first attribs to PlayerController class

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D _rigidBody;
+    private bool _moveForward = false;
+    private float _speed = 0.05f;
+
+    private void Awake()
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World, Duck!");
     }
 
     // Update is called once per frame
