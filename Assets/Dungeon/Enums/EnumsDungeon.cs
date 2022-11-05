1bf2019fa68650b2ceed16ef7faa36a4d70f3efb
katema-official <alessio.lagreca13@gmail.com>
2022-11-05 11:45:42 +0100

    Added fields and enums

---
+++ Assets/Dungeon/Enums/EnumsDungeon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelStageNamespace
+{
+
+    public class EnumsDungeon
+    {
+        public enum CompassDirection
+        {
+            North,
+            South,
+            West,
+            East
+        };
+
+        public enum LakeDimension
+        {
+            Small,
+            Medium,
+            Large
+        }
+
+        public enum BreadType
+        {
+            Small,
+            Medium,
+            Large
+        }
+
+    }
+
+}
--- Assets/Dungeon/Scripts/Lakes/LakeDescriptionComponent.cs
+++ Assets/Dungeon/Scripts/Lakes/LakeDescriptionComponent.cs
@@ -2,35 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LakeDescriptionComponent : MonoBehaviour
-{
-    //component that specifies the nature of a lake
-    [SerializeField] public int NumberOfMallard;
-    [SerializeField] public int NumberOfCoot;
-    [SerializeField] public int NumberOfGoose;
-    [SerializeField] public int NumberOfFish;       //we'll think about them in the future
-    [SerializeField] public int NumberOfSeagull;    //we'll think about them in the future
-
-
-
-
-
-
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+namespace LevelStageNamespace {
+    public class LakeDescriptionComponent : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        //component that specifies the nature of a lake
+
         
+
+
+        [SerializeField] public int NumberOfMallard;
+        [SerializeField] public int NumberOfCoot;
+        [SerializeField] public int NumberOfGoose;
+        [SerializeField] public int NumberOfFish;       //we'll think about them in the future
+        [SerializeField] public int NumberOfSeagull;    //we'll think about them in the future
+
+        [SerializeField] private Transform NorthRiver;
+        [SerializeField] private Transform SouthRiver;
+        [SerializeField] private Transform WestRiver;
+        [SerializeField] private Transform EastRiver;
+
+        [Header("From where does the player come? North, south, east or west?")]
+        [SerializeField] public EnumsDungeon.CompassDirection SpawnPlayer;
+        [SerializeField] public float EntranceTimer;
+
+        [SerializeField] public EnumsDungeon.LakeDimension Dimension;
+
+        public Dictionary<EnumsDungeon.BreadType, int> BreadToSpawnMap;
+
+        private int _totalNumberOfBreadPiecesToSpawn;       //can be obtained scanning BreadToSpawnMap
+        private int _totalNumberOfBreadPiecesToBeEaten;     //it's _totalNumberOfBreadPiecesToSpawn initially, but can get bigger if a new piece of bread is spawned because of stealing, for example
+        private int _totalNumberOfBreadPiecesEaten;         //counts how many pieces of bread have been eaten in this lake
+
+        private float _arrayBreadSpawnTime;
+        private float _minIntervalTimeSpawnBread;
+        private float _maxIntervaltimeSpawnBread;
+
+        [SerializeField] public bool LakeCleared;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var so = ScriptableObject.CreateInstance<StageSO>();
+            so.MinNumberOfLakes = 3;
+            Debug.Log("hjnifsv = " + so.MinNumberOfLakes);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
+
+
