8f76850ea2aeaca71a2490ad1bfb4a3811fc8668
ivs-spec <ivs-spec@github.com>
2022-11-06 17:32:52 +0100

    Added further features to the Player: Base Sprite, Main Duck Parts handled inside the controller.

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _rigidBody;
+    private Rigidbody2D _rigidBody = null;
+    private Camera _camera = null;
+    private Transform _mouth = null;
+
     private bool _moveForward = false;
-    private float _speed = 0.05f;
+    private float _speed = 0.0f;
 
     private void Awake()
     {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _camera = transform.parent.GetComponentInChildren<Camera>();
+        _mouth = transform.Find("Mouth");
     }
 
     // Start is called before the first frame update
