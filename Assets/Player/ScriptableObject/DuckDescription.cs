2d47200a04ac979ef394987d6e5f0a19ee6dc543
ivs-spec <ivs-spec@github.com>
2022-11-14 17:17:18 +0100

    Added Duck Description

---
+++ Assets/Player/ScriptableObject/DuckDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Duck/DuckDescription")]
+public class DuckDescription : ScriptableObject
+{
+    public string Name = "";
+    public string Description = "";
+    public float Speed = 0.0f;
+    public float EatingSpeed = 0.0f;
+    public float ChewingRate = 0.0f;
+    public int MouthSize = 0;
+}
