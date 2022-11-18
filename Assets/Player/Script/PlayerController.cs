9fbd8af3cc3eec17fb170b8e4c9798c9a37f649c
ivs-spec <ivs-spec@github.com>
2022-11-18 11:44:17 +0100

    Moved entire move logic in the move skill module

--- Assets/Player/Script/PlayerController.cs
+++ Assets/Player/Script/PlayerController.cs
@@ -10,16 +10,16 @@ namespace Player
         private Camera _camera = null;
         private Transform _mouth = null;
 
-        private LakeController _currentLake = null;
+        //private LakeController _currentLake = null;
 
-        private Vector3 _forwardAxis;
-        private Vector3 _rightwardAxis;
+      //  private Vector3 _forwardAxis;
+      //  private Vector3 _rightwardAxis;
 
-        private bool _moveForward = false;
-        private float _rotationMovement = 0.0f;
-      //  private float _rotationSpeed = 90.0f;
+      //  private bool _moveForward = false;
+      //  private float _rotationMovement = 0.0f;
+      ////  private float _rotationSpeed = 90.0f;
 
-        private float _force = 0.0f;
+      //  private float _force = 0.0f;
         private PlayerState _state = PlayerState.Normal;
 
 
@@ -30,54 +30,61 @@ namespace Player
 
         //Derived from DuckDescription
         //---------------------------------
-        [SerializeField] private float _speed = 0.0f;
+        // [SerializeField] private float _speed = 0.0f;
         //[SerializeField] private float _eatingSpeed = 0.0f;
         //[SerializeField] private float _chewingRate = 0.0f;
         //[SerializeField] private int _mouthSize = 0;
         //---------------------------------
 
-        public void Move(float speed, float rotationMovement, bool moveForward)
+        //public void Move(float speed, float rotationMovement, bool moveForward)
+        //{
+        //    if (moveForward)
+        //    {   
+        //        var finalDir = _forwardAxis + _rightwardAxis;
+        //        finalDir.Normalize();
+
+        //        float angle = Mathf.Atan2(-finalDir.x, finalDir.y) * Mathf.Rad2Deg;
+        //        _rotationMovement = angle;
+        //        _force = _speed * 1.5f;
+        //        _rigidBody.AddForce(finalDir * _force, ForceMode2D.Force);
+        //        _rigidBody.velocity = Vector2.ClampMagnitude(_rigidBody.velocity, _speed);
+        //    }
+
+        //    _rigidBody.SetRotation(Quaternion.AngleAxis(_rotationMovement, Vector3.forward));
+
+        //     Debug.Log("Current player velocity: " + _rigidBody.velocity);
+
+
+
+        //}
+
+
+        public PlayerState GetState()
         {
-            if (moveForward)
-            {   
-                var finalDir = _forwardAxis + _rightwardAxis;
-                finalDir.Normalize();
-           
-                float angle = Mathf.Atan2(-finalDir.x, finalDir.y) * Mathf.Rad2Deg;
-                _rotationMovement = angle;
-                _force = _speed * 1.5f;
-                _rigidBody.AddForce(finalDir * _force, ForceMode2D.Force);
-                _rigidBody.velocity = Vector2.ClampMagnitude(_rigidBody.velocity, _speed);
-            }
-
-            _rigidBody.SetRotation(Quaternion.AngleAxis(_rotationMovement, Vector3.forward));
-
-             Debug.Log("Current player velocity: " + _rigidBody.velocity);
-
-
-            
+            return _state;
         }
 
-        private void MoveCamera()
-        {
-            _camera.transform.position = _rigidBody.position;
-            var cameraBounds = CameraUtility.OrthographicBounds(_camera);
-            Bounds lakeBounds;
 
-            if (_currentLake)
-                lakeBounds = _currentLake.getBounds();
-            else
-                lakeBounds = new Bounds();
+        //private void MoveCamera()
+        //{
+        //    _camera.transform.position = _rigidBody.position;
+        //    var cameraBounds = CameraUtility.OrthographicBounds(_camera);
+        //    Bounds lakeBounds;
 
-            Vector3 newCamPos = transform.position;
+        //    if (_currentLake)
+        //        lakeBounds = _currentLake.getBounds();
+        //    else
+        //        lakeBounds = new Bounds();
 
-            if (cameraBounds.min.x < lakeBounds.min.x) newCamPos.x += lakeBounds.min.x - cameraBounds.min.x;
-            if (cameraBounds.max.x > lakeBounds.max.x) newCamPos.x -= cameraBounds.max.x - lakeBounds.max.x;
-            if (cameraBounds.min.y < lakeBounds.min.y) newCamPos.y += lakeBounds.min.y - cameraBounds.min.y;
-            if (cameraBounds.max.y > lakeBounds.max.y) newCamPos.y -= cameraBounds.max.y - lakeBounds.max.y;
+        //    Vector3 newCamPos = transform.position;
 
-            _camera.transform.position = newCamPos;
-        }
+        //    if (cameraBounds.min.x < lakeBounds.min.x) newCamPos.x += lakeBounds.min.x - cameraBounds.min.x;
+        //    if (cameraBounds.max.x > lakeBounds.max.x) newCamPos.x -= cameraBounds.max.x - lakeBounds.max.x;
+        //    if (cameraBounds.min.y < lakeBounds.min.y) newCamPos.y += lakeBounds.min.y - cameraBounds.min.y;
+        //    if (cameraBounds.max.y > lakeBounds.max.y) newCamPos.y -= cameraBounds.max.y - lakeBounds.max.y;
+
+        //    _camera.transform.position = newCamPos;
+        //}
 
         private void Awake()
         {
@@ -97,7 +104,7 @@ namespace Player
             //_mouthSize = _description.MouthSize;        
         
 
-            _force = _speed * 1.5f;
+            //_force = _speed * 1.5f;
             _camera = transform.parent.GetComponentInChildren<Camera>();
             _mouth = transform.Find("Mouth");
 
@@ -119,49 +126,49 @@ namespace Player
         // Update is called once per frame
         private void Update()
         {
-            _moveForward = false;
-            _forwardAxis = new Vector3(0, 0);
-            _rightwardAxis = new Vector3(0, 0);
+            //_moveForward = false;
+            //_forwardAxis = new Vector3(0, 0);
+            //_rightwardAxis = new Vector3(0, 0);
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                _forwardAxis = new Vector3(0, 1);
-                _moveForward = true;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                _rightwardAxis = new Vector3(-1, 0);
-                _moveForward = true;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                _forwardAxis = new Vector3(0, -1);
-                _moveForward = true;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                _rightwardAxis = new Vector3(1, 0);
-                _moveForward = true;
-            }
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    _forwardAxis = new Vector3(0, 1);
+            //    _moveForward = true;
+            //}
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    _rightwardAxis = new Vector3(-1, 0);
+            //    _moveForward = true;
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    _forwardAxis = new Vector3(0, -1);
+            //    _moveForward = true;
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    _rightwardAxis = new Vector3(1, 0);
+            //    _moveForward = true;
+            //}
         }
 
 
         private void FixedUpdate()
         {
-            if (_state == PlayerState.Normal)  Move(_speed, _rotationMovement, _moveForward);
+            //if (_state == PlayerState.Normal)  Move(_speed, _rotationMovement, _moveForward);
            // MoveCamera();
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
-            if (lakeController)  _currentLake = lakeController;
-        }
+        //private void OnTriggerEnter2D(Collider2D collision)
+        //{
+        //    var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
+        //    if (lakeController)  _currentLake = lakeController;
+        //}
 
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
-            if (lakeController)  _currentLake = null;
-        }
+        //private void OnTriggerExit2D(Collider2D collision)
+        //{
+        //    var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
+        //    if (lakeController)  _currentLake = null;
+        //}
     }
 }
--- Assets/Player/Script/Skill/PlayerMoveSkill.cs
+++ Assets/Player/Script/Skill/PlayerMoveSkill.cs
@@ -6,7 +6,52 @@ namespace Player
 {
     public class PlayerMoveSkill : PlayerSkill
     {
-        // Start is called before the first frame update
+        [SerializeField] private float _speed = 0.0f;
+
+
+
+
+        private PlayerController _controller = null;
+
+        private Rigidbody2D _rigidBody = null;
+        private Vector3 _forwardAxis;
+        private Vector3 _rightwardAxis;
+
+        private bool _moveForward = false;
+        private float _rotationMovement = 0.0f;
+
+        private float _force = 0.0f;
+
+
+
+        private void Move(float speed, float rotationMovement, bool moveForward)
+        {
+            if (moveForward)
+            {
+                var finalDir = _forwardAxis + _rightwardAxis;
+                finalDir.Normalize();
+
+                float angle = Mathf.Atan2(-finalDir.x, finalDir.y) * Mathf.Rad2Deg;
+                _rotationMovement = angle;
+                _force = _speed * 1.5f;
+                _rigidBody.AddForce(finalDir * _force, ForceMode2D.Force);
+                _rigidBody.velocity = Vector2.ClampMagnitude(_rigidBody.velocity, _speed);
+            }
+
+            _rigidBody.SetRotation(Quaternion.AngleAxis(_rotationMovement, Vector3.forward));
+
+            Debug.Log("Current player velocity: " + _rigidBody.velocity);
+        }
+
+
+        private void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+            _controller = GetComponent<PlayerController>();
+
+            var duckTypeManager = GameObject.FindObjectOfType<DuckTypeManager>();
+        }
+            // Start is called before the first frame update
         void Start()
         {
         
@@ -15,7 +60,40 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
-        
+            if (_controller.GetState() != PlayerState.Normal) return;
+
+            _moveForward = false;
+            _forwardAxis = new Vector3(0, 0);
+            _rightwardAxis = new Vector3(0, 0);
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                _forwardAxis = new Vector3(0, 1);
+                _moveForward = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _rightwardAxis = new Vector3(-1, 0);
+                _moveForward = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _forwardAxis = new Vector3(0, -1);
+                _moveForward = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _rightwardAxis = new Vector3(1, 0);
+                _moveForward = true;
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (_controller.GetState() != PlayerState.Normal) return;
+
+            Move(_speed, _rotationMovement, _moveForward);
         }
     }
 }
