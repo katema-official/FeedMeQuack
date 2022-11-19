e757f1f7390d97392f48fb159d14941ef06f243f
ivs-spec <ivs-spec@github.com>
2022-11-19 10:32:07 +0100

    Implementing Dash Skill

--- Assets/Player/Script/Skill/PlayerDashSkill.cs
+++ Assets/Player/Script/Skill/PlayerDashSkill.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerDashSkill : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerDashSkill : PlayerSkill
     {
-        
-    }
+        //Dash Skill Data
+        //------------------------------------------
+        [SerializeField]  private float _maxSpeed = 0.0f;
+        [SerializeField] private float _maxDuration = 0.0f;
+        [SerializeField] private float _coolDown = 0.0f;
+        //-------------------------------------
+        private float _dashElapsedSeconds = 0.0f;
+        private float _dashCoolDownElapsedSeconds = 0.0f;
+        //-------------------------------------
 
-    // Update is called once per frame
-    void Update()
-    {
+
+        private PlayerController _controller = null;
+        private PlayerMoveSkill _moveSkill = null;
+        private PlayerDashSkillDescriptionSO _dashDesc = null;
         
+        
+        public override void SetDescription(PlayerSkillDescriptionSO desc)
+        {
+            _description = desc;
+            _dashDesc = (PlayerDashSkillDescriptionSO)_description;
+
+            _maxSpeed = _dashDesc.MaxSpeed;
+            _maxDuration = _dashDesc.MaxDuration;
+            _coolDown = _dashDesc.CoolDown;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
     }
 }
