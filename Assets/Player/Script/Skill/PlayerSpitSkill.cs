5b0e5f9344cf94e0961b11a2ca82f7c0cb1c289f
ivs-spec <ivs-spec@github.com>
2022-11-23 18:32:35 +0100

    Added Spit Skill class

---
+++ Assets/Player/Script/Skill/PlayerSpitSkill.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerSpitSkill : PlayerSkill
+    {
+        //Spit Skill Data
+        //------------------------------------------
+        [SerializeField] private float _maxPower = 0.0f;
+        [SerializeField] private float _maxRange = 0.0f;
+        [SerializeField] private float _coolDown = 0.0f;
+        [SerializeField] private float _chargeSpeed = 0.0f;
+        //------------------------------------------
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
