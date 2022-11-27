1b45850fd9283c908ec0ebe3dfd6c48ed0f51ef6
ivs-spec <ivs-spec@github.com>
2022-11-27 17:33:33 +0100

    Added Steal Skill class

---
+++ Assets/Player/Script/Skill/PlayerStealSkill.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerSpitSkill : PlayerSkill
+    {
+        //Steal Skill Data
+        //------------------------------------------
+        [SerializeField] private float _maxPower = 0.0f;
+        //------------------------------------------
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
