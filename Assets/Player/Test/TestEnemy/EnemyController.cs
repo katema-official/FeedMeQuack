2e51ae007a56ea53b177631641639b83bec309c9
ivs-spec <ivs-spec@github.com>
2022-11-27 16:51:44 +0100

    added test fake enemies

--- Assets/Player/Test/TestEnemy/EnemyController.cs
+++ Assets/Player/Test/TestEnemy/EnemyController.cs
@@ -9,6 +9,16 @@ namespace Player
 {
     public class EnemyController : MonoBehaviour
     {
+        private LakeController _currentLake = null;
+        private BreadController _catchedBread = null;
+        private Transform _mouth = null;
+
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _mouth = transform.Find("Mouth");
+        }
         // Start is called before the first frame update
         void Start()
         {
@@ -18,7 +28,23 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
-        
+            
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
+            if (lakeController) {
+                _currentLake = lakeController;
+                _catchedBread = _currentLake.GenerateNewBread();
+                _catchedBread.Move(_mouth.position);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            var lakeController = collision.gameObject.GetComponentInChildren<LakeController>();
+            if (lakeController) _currentLake = null;
         }
     }
 }
