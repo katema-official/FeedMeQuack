e209e354ea93bfa2dc08776e61503aa51d2ea4d7
ivs-spec <ivs-spec@github.com>
2022-11-29 19:18:06 +0100

    fixed spit throw

--- Assets/Player/Manager/PlayerBreadManager.cs
+++ Assets/Player/Manager/PlayerBreadManager.cs
@@ -9,12 +9,12 @@ namespace Player
     {
         [SerializeField] private GameObject BreadToThrow;
 
-        public void ThrowBread(LevelStageNamespace.EnumsDungeon.BreadType breadType, int breadPoints, Vector3 startPos, Vector3 endPos)
+        public void ThrowBread(BreadNamespace.BreadInMouthComponent breadToThrow, Vector3 startPos, Vector3 endPos)
         {
             GameObject newBread = Instantiate(BreadToThrow);
             newBread.GetComponent<BreadNamespace.BreadThrownComponent>().InitializeBreadThrownFromDuck(
-                breadType,
-                //breadPoints,
+                LevelStageNamespace.EnumsDungeon.BreadType.Medium,
+                //breadToThrow,
                 startPos.x,startPos.y,
                  endPos.x, endPos.y,10,30);
         }
