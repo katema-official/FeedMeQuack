6c887806bddd142c8f4774f5cb4ca4d1e033f4f4
ivs-spec <ivs-spec@github.com>
2022-12-06 11:05:04 +0100

    added skill attribute enum

---
+++ Assets/Player/Script/Skill/PlayerSkillAttribute.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public enum PlayerSkillAttribute
+    {
+        MoveSkill_Speed,
+
+        EatSkill_EatingSpeed,
+        EatSkill_ChewingRate,
+        EatSkill_MouthSize,
+
+
+        DashSkill_MaxSpeed,
+        DashSkill_MaxDuration,
+        DashSkill_CoolDown,
+
+        SpitSkill_MaxPower,
+        SpitSkill_MaxRange,
+        SpitSkill_CoolDown,
+        SpitSkill_ChargeSpeed,
+
+        StealSkill_CoolDown,
+    }
+
+}
