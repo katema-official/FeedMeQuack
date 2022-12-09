c77b2404249fb6143a27dc7a94192d385f850714
katema-official <alessio.lagreca13@gmail.com>
2022-12-09 10:57:45 +0100

    mid work generation of obstacles for small lake

--- Assets/Dungeon/Scripts/Lakes/Obstacles/ObstaclesLakeSmallComponent.cs
+++ Assets/Dungeon/Scripts/Lakes/Obstacles/ObstaclesLakeSmallComponent.cs
@@ -8,7 +8,9 @@ public class ObstaclesLakeSmallComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateObstaclesDescription();
+        (int setIndex, List<(int, int)> quadrants_obstacles) = GenerateObstaclesDescription();
+        //TO REMOOOOOOOVE
+        SetObstacles(setIndex, quadrants_obstacles);
     }
 
     // Update is called once per frame
@@ -46,22 +48,45 @@ public class ObstaclesLakeSmallComponent : MonoBehaviour
                 {2, new List<int>(){1,2,3,4} }
             }
         },
+
+        {4,  new Dictionary<int, List<int>>()
+            {
+                {1, new List<int>{1} }
+            }
+        },
     };
 
+    //the probability, for each set, to be chosen, Must sum up to one
+    private static List<float> _probabilitiesOfSets = new List<float>() { 0.60f, 0.10f, 0.25f, 0.05f };
 
 
 
-
+    //a static method used to choose the obstacles that we want to place in the lake small
     public static (int, List<(int, int)>) GenerateObstaclesDescription()
     {
-        
         //first of all, we have to choose a random ObstacleSet
-        int setIndex = Random.Range(1, _nObstaclesSet + 1);
+        float v = Random.Range(0f, 1f);
+        bool chose = false;
+        int idx = 0;
+        float sum = _probabilitiesOfSets[idx];
+        while (!chose)
+        {
+            if(v <= sum)
+            {
+                chose = true;
+            }
+            else
+            {
+                idx++;
+                sum += _probabilitiesOfSets[idx];
+            }
+        }
+        int setIndex = idx + 1; //because our sets go from 1 to n
 
-        //then, I have to choose which quadrants do I want to activate
+        //then, I have to choose which quadrants do I want to activate (at least one, ok?)
         Dictionary<int, List<int>> quadrantsDict = _obstacleSetsDictionary[setIndex];
         List<int> quadrantsKeys = quadrantsDict.Keys.ToList<int>();
-        List<int> quadrantsActiveIndexes = quadrantsKeys.OrderBy(x => Random.Range(0f, 1f)).Take(Random.Range(0, quadrantsKeys.Count + 1)).ToList<int>();
+        List<int> quadrantsActiveIndexes = quadrantsKeys.OrderBy(x => Random.Range(0f, 1f)).Take(Random.Range(1, quadrantsKeys.Count + 1)).ToList<int>();
 
         //for each of these quadrants, I have to choose one Obs
         List<int> obsChosenInOrder = new List<int>();
@@ -70,29 +95,51 @@ public class ObstaclesLakeSmallComponent : MonoBehaviour
             int obs = Random.Range(1, quadrantsDict[i].Count + 1);
             obsChosenInOrder.Add(obs);
         }
-        Debug.Log("In the end, we chose set " + setIndex + "with the following quadrants-obs:");
+
+        List<(int, int)> quadrants_obstacles = new List<(int, int)>();
+        //Debug.Log("In the end, we chose set " + setIndex + "with the following quadrants-obs:");
         for(int i = 0; i < quadrantsActiveIndexes.Count; i++)
         {
-            Debug.LogFormat("Q {0} -> Obs {1}", quadrantsActiveIndexes[i], obsChosenInOrder[i]);
-
+            quadrants_obstacles.Add((quadrantsActiveIndexes[i], obsChosenInOrder[i]));
+            //Debug.LogFormat("Q {0} -> Obs {1}", quadrants_obstacles[i].Item1, quadrants_obstacles[i].Item2);
         }
 
+        
 
-        /*List<int> my = new List<int>() {1,2,3,4,5};
-        List<int> ret = my.OrderBy(x => Random.Range(0f,1f)).Take(Random.Range(0, my.Count + 1)).ToList<int>();
-        foreach(int i in ret)
-        {
-            Debug.Log(i);
-        }
-        Debug.Log(ret);*/
-
-
-        return (0, new List<(int, int)>());
-
-
-
+        return (setIndex, quadrants_obstacles);
     }
 
 
 
+    //method to apply the given set of obstacles, and only them
+    public void SetObstacles(int setIndex, List<(int, int)> quadrants_obstacles)
+    {
+        //first of all: disable all child gameobjects
+        foreach(Transform child in transform)
+        {
+            DisableGameobjectRecursively(child.gameObject);
+        }
+
+        //then, re-activate only the right ones
+        string obstacleSetSTRING = "ObstacleSet" + setIndex;
+        gameObject obstacleSetGO = transform.Find(obstacleSetSTRING).gameObject;
+        .SetActive(true);
+
+        foreach((int, int) quadrant_obs in quadrants_obstacles)
+        {
+
+        }
+
+
+    }
+
+    //method to disable a gameobject and ALL of its children recursively
+    private void DisableGameobjectRecursively(GameObject go)
+    {
+        foreach(Transform child in go.transform)
+        {
+            DisableGameobjectRecursively(child.gameObject);
+        }
+        go.SetActive(false);
+    }
 }
