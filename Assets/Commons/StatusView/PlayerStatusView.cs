1ea7749be41c729d12107f5ee27342eee15a8732
ivs-spec <ivs-spec@github.com>
2022-12-23 12:15:08 +0100

    Implementing Common Status View

---
+++ Assets/Commons/StatusView/PlayerStatusView.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+namespace Player
+{
+    public class PlayerStatusView : MonoBehaviour
+    {
+        private Image _icon = null;
+        private TextMeshProUGUI _label = null;
+
+        public void SetIcon(Sprite icon)
+        {
+            _icon.sprite = icon;
+        }
+        public void SetText(string text)
+        {
+            _label.text = text;
+        }
+        public void SetPosition(Vector2 pos)
+        {
+            GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+
+        public void SetVisible(bool visible)
+        {
+            gameObject.SetActive(visible);
+        }
+        void Awake()
+        {
+            _icon = transform.Find("StatusIcon").GetComponent<Image>();
+            _label = transform.Find("ValueText").GetComponent<TextMeshProUGUI>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
