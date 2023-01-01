c740f1424e76ef9b92287dc3227b9c388264f9da
ivs-spec <ivs-spec@github.com>
2023-01-01 17:04:33 +0100

    Added scripts for buttons and manager gameover

---
+++ Assets/GameOver/Scripts/GOButton.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class GOButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+
+
+    private bool _enable = false;
+    private Image _image = null;
+    private TextMeshProUGUI _text = null;
+    private GOButtonManager _manager = null;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _manager.SetEnableButtons(false);
+        SetEnable(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _manager.SetEnableButtons(false);
+        SetEnable(false);
+    }
+    public void SetEnable(bool enable)
+    {
+        if (enable)
+        {
+            _image.gameObject.SetActive(true);
+            _text.fontStyle = FontStyles.Underline;
+            _text.color = new Color(100.0f / 255.0f, 61.0f / 255.0f, 22.0f / 255.0f);
+        }
+        else
+        {
+            _image.gameObject.SetActive(false);
+            _text.fontStyle = FontStyles.Normal;
+            _text.color = Color.white;
+        }
+        _enable = enable;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _image = transform.GetComponentInChildren<Image>();
+        _text = transform.GetComponentInChildren<TextMeshProUGUI>();
+        _manager = transform.parent.GetComponent<GOButtonManager>();
+       // SetEnable(false);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameOver/Scripts/GOButtonManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOButtonManager : MonoBehaviour
+{
+
+    public void SetEnableButtons(bool enable)
+    {
+        foreach (Transform c in transform)
+        {
+            c.GetComponent<GOButton>().SetEnable(enable);
+        }
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+      
+
+
+
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetEnableButtons(false);
+        //foreach(Transform c in transform)
+        //{
+        //    c.GetComponent<GOButton>().SetEnable(false);
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
