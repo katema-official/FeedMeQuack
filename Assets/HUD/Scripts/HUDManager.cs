f5aefd193e0e7eeed11878d5f54b72b632d79bf9
MarcoDottor <marco.dottor@mail.polimi.it>
2023-01-02 11:54:03 +0100

    Skills hud implemented,

--- Assets/HUD/Scripts/HUDManager.cs
+++ Assets/HUD/Scripts/HUDManager.cs
@@ -54,13 +54,34 @@ namespace HUDNamespace
             switch (skillType)
             {
                 case textFields.dashCD:
-                    dashCDValueText.text = newValue.ToString();
+                    dashCDValueText.text = newValue.ToString()+" sec.";
+                    if (newValue == 0){
+                        dashCDValueText.text = "Available";
+                        dashCDValueText.color= Color.green;
+                    }
+                    else{
+                        dashCDValueText.color= Color.white;
+                    }
                     break;
                 case textFields.stealCD:
-                    stealCDValueText.text = newValue.ToString();
+                    stealCDValueText.text = newValue.ToString()+" sec.";
+                    if (newValue == 0){
+                        stealCDValueText.text = "Available";
+                        stealCDValueText.color= Color.green;
+                    }
+                    else{
+                        stealCDValueText.color= Color.white;
+                    }
                     break;
                 case textFields.spitCD:
-                    spitCDValueText.text = newValue.ToString();
+                    spitCDValueText.text = newValue.ToString()+" sec.";
+                    if (newValue == 0){
+                        spitCDValueText.text = "Available";
+                        spitCDValueText.color= Color.green;
+                    }
+                    else{
+                        spitCDValueText.color= Color.white;
+                    }
                     break;
             }
         }
