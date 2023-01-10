cb68ea5ebaa3cfb78ced766689a97bd689396199
Alessio La Greca <alessio.lagreca13@gmail.com>
2023-01-10 16:06:07 +0100

    new version

--- Assets/Music/Scripts/DisclaimerController.cs
+++ Assets/Music/Scripts/DisclaimerController.cs
@@ -8,7 +8,7 @@ namespace Music
     public class DisclaimerController : MonoBehaviour
     {
         private const float _timeForMainMenu = 15f;
-        private const string _version = "1.0.2";
+        private const string _version = "1.0.3";
 
         public static string GetGameVersion()
         {
